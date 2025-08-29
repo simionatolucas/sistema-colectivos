794dc133e29fcf542f3a2e6cae4808b38d93a964
Lucas Simionato <simionato.lucas99@gmail.com>
2025-08-29 18:41:31 -0300

    first commit

---
+++ src/main/java/com/uncuyo/sistemacolectivos/SistemaColectivos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uncuyo.sistemacolectivos;
+
+/**
+ *
+ * @author lsi99
+ */
+public class SistemaColectivos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
