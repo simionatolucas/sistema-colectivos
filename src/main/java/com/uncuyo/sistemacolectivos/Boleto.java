f68e3975357126bfba23400b8a0eb291b8901788
Lucas Simionato <simionato.lucas99@gmail.com>
2025-08-29 20:33:48 -0300

    fix boleto

--- src/main/java/com/uncuyo/sistemacolectivos/Boleto.java
+++ src/main/java/com/uncuyo/sistemacolectivos/Boleto.java
@@ -2,22 +2,22 @@ import java.util.UUID;
 
 class Boleto {
     private UUID idBoleto;
-    public DateTime fechaEmision;
+    public String fechaEmision;
     public float precio;
     public UUID pasajero;
 
-    public Boleto(DateTime fechaEmision, float precio, UUID pasajero) {
+    public Boleto(String fechaEmision, float precio, UUID pasajero) {
         this.idBoleto = UUID.randomUUID();
         this.fechaEmision = fechaEmision;
         this.precio = precio;
         this.pasajero = pasajero; 
     }
 
-    public String verDetalles() {
-        System.out.println("Mostrando detalles de la tarjeta %s asociada al pasajero %s", idBoleto, pasajero);
+    public void verDetalles() {
+        System.out.println(String.format("Mostrando detalles de la tarjeta %s asociada al pasajero %s", this.idBoleto, this.pasajero));
     }
 
-    public String getId() {
-        return idBoleto;
+    public UUID getId() {
+        return this.idBoleto;
     }
 }
